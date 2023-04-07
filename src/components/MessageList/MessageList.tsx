2e7dfb06a5fd7b5e9d46b569cc44e3a261cc31b1
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-04-08 01:39:47 +0300

    add server for users and styles changes

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import AddMessage from "../AddMessage/AddMessage";
-import  "./MessageList.module.scss";
+import styles from  "./MessageList.module.scss";
 
 export default function MessageList(): JSX.Element {
   const [messages, setMessages] = useState<string[]>([]);
@@ -15,7 +15,7 @@ export default function MessageList(): JSX.Element {
     <section id="messages_list">
       <ul style={{ listStyleType: "none", padding: 10, margin: 0 }}>
         {messages.map((message, index) => (
-          <li key={index}>{message}</li>
+          <li className={styles.list_li} key={index}>{message}</li>
         ))}
       </ul>
       <AddMessage onAddMessage={addMessage} />
