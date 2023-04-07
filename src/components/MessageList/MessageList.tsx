83e7c0bfbdd667f1c894763a323f0739a18b8564
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-04-07 23:09:35 +0300

    remove ul dots

--- src/components/MessageList/MessageList.tsx
+++ src/components/MessageList/MessageList.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AddMessage from "../AddMessage/AddMessage";
+import  "./MessageList.module.scss";
 
 export default function MessageList(): JSX.Element {
   const [messages, setMessages] = useState<string[]>([]);
@@ -12,7 +13,7 @@ export default function MessageList(): JSX.Element {
 
   return (
     <section id="messages_list">
-      <ul>
+      <ul style={{ listStyleType: "none", padding: 10, margin: 0 }}>
         {messages.map((message, index) => (
           <li key={index}>{message}</li>
         ))}
