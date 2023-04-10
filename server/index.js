d608a371358e16608138d406b1162cf4cdc63e97
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-04-10 21:25:48 +0300

    removed genereting random names

--- server/index.js
+++ server/index.js
@@ -10,36 +10,22 @@ const io = socketIO(server, {
   }
 });
 
-const adjectives = ['happy', 'silly', 'funny', 'clever', 'wise', 'smart', 'brave'];
-const nouns = ['cat', 'dog', 'hamster', 'elephant', 'lion', 'tiger', 'penguin'];
-
 // Keep track of connected users
 let connectedUsers = [];
 
-function generateRandomName() {
-  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
-  return `${adjective}-${noun}`;
-}
+
 
 io.on('connection', (socket) => {
 
-  const name = generateRandomName();
-  console.log(name + ' connected');
-
-  // Add user to connectedUsers array
-  connectedUsers.push(name);
-
-  // Emit updated connectedUsers array to all connected clients
-  io.emit('users', connectedUsers);
-
-  socket.emit('name', name);
+  socket.on('new user', (name) => {
+    socket.name = name;
+    connectedUsers.push(name);
+    io.emit('users', connectedUsers);
+  });
 
   socket.on('disconnect', () => {
-    console.log(name + ' disconnected');
-    // Remove user from connectedUsers array
-    connectedUsers = connectedUsers.filter(user => user !== name);
-    // Emit updated connectedUsers array to all connected clients
+    console.log(socket.name + ' disconnected');
+    connectedUsers = connectedUsers.filter(user => user !== socket.name);
     io.emit('users', connectedUsers);
   });
 });
