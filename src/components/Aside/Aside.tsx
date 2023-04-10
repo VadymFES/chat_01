11d86d16f7ad95750b1a6b9780a8d552dbc41386
[Vadym Fesiuk] <[vadymfesiuk@gmail.com]>
2023-04-10 21:26:36 +0300

    add names input for users

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -7,11 +7,45 @@ interface User {
   name: string;
 }
 
+interface Props {
+  onNameSubmit: (name: string) => void;
+}
+
+function NamePrompt({ onNameSubmit }: Props): JSX.Element {
+  const [name, setName] = useState('');
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (name) {
+      onNameSubmit(name);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter your name:
+          <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
 export default function Aside(): JSX.Element {
   const [users, setUsers] = useState<User[]>([]);
+  const [showNamePrompt, setShowNamePrompt] = useState(true);
 
   useEffect(() => {
     const socket = io('http://localhost:3001');
+
+    const storedName = localStorage.getItem('userName');
+    if (storedName) {
+      socket.emit('new user', storedName);
+      setShowNamePrompt(false);
+    }
 
     socket.on('users', (users: string[]) => {
       setUsers(users.map((name, index) => ({ id: index.toString(), name })));
@@ -22,9 +56,17 @@ export default function Aside(): JSX.Element {
     };
   }, []);
 
+  function handleNameSubmit(name: string) {
+    setShowNamePrompt(false);
+    const socket = io('http://localhost:3001');
+    socket.emit('new user', name);
+    localStorage.setItem('userName', name);
+  }
+
   return (
     <aside className={styles.aside}>
       <h1>Users</h1>
+      {showNamePrompt && <NamePrompt onNameSubmit={handleNameSubmit} />}
       <ul>
         {users.map(user => (
           <li key={user.id}>{user.name}</li>
