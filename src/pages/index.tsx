860dc91693d68db683545558e3d8c40fa0650022
Vadym Fesiuk <vadymfesiuk@gmail.com>
2024-01-07 16:59:13 +0100

    fixed imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
 import Aside from '@/components/Aside/Aside'
 import MessageList from '@/components/MessageList/MessageList'
 
